40d19251cdb091a00aa1d0017e6bb3a3b6e811e8
tonminhce <minh.nguyentomy2412@hcmut.edu.vn>
2026-08-27 16:01:50 +0700

    feat(product): add Brand entity

---
+++ product-service/src/main/java/com/shop/productservice/entity/Brand.java
@@ -0,0 +1,32 @@
+package com.shop.productservice.entity;
+
+import com.shop.common.core.data.AbstractMappedEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "brands")
+@SQLRestriction("deleted = false")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Brand extends AbstractMappedEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String slug;
+
+    @Column(name = "logo_url", length = 500)
+    private String logoUrl;
+
+    @Column(length = 1000)
+    private String description;
+}
