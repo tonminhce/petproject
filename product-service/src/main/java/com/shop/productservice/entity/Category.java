d7efbf5620c26e9c4f1df38f197c5bea3012162a
tonminhce <minh.nguyentomy2412@hcmut.edu.vn>
2026-08-27 16:01:47 +0700

    feat(product): add Category entity (extends AbstractMappedEntity, @SQLRestriction)

---
+++ product-service/src/main/java/com/shop/productservice/entity/Category.java
@@ -0,0 +1,41 @@
+package com.shop.productservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.shop.common.core.data.AbstractMappedEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@SQLRestriction("deleted = false")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category extends AbstractMappedEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String slug;
+
+    @Column(name = "image_url", length = 500)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    private Set<Category> children = new HashSet<>();
+}
