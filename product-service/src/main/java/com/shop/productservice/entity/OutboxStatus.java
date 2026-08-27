b412497c0f9ec22e71d26383d9704bea46c9c7ea
tonminhce <minh.nguyentomy2412@hcmut.edu.vn>
2026-08-27 16:01:39 +0700

    feat(product): add ProductStatus + OutboxStatus enums

---
+++ product-service/src/main/java/com/shop/productservice/entity/OutboxStatus.java
@@ -0,0 +1,5 @@
+package com.shop.productservice.entity;
+
+public enum OutboxStatus {
+    PENDING, SENT, FAILED
+}
---
+++ product-service/src/main/java/com/shop/productservice/entity/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.shop.productservice.entity;
+
+public enum ProductStatus {
+    DRAFT, ACTIVE, OUT_OF_STOCK, DISCONTINUED
+}
