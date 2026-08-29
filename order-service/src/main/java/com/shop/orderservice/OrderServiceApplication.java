030f8b93f69027e89826f14e88acc633efa2bdf3
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2026-08-29 10:13:20 +0700

    feat(order-service): application entrypoint + application.yml

--- order-service/src/main/java/com/shop/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/shop/orderservice/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.shop.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling  // REQUIRED: OutboxRelay + OutboxRetentionScheduler + IdempotencyKeyCleanupScheduler
 public class OrderServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
-
 }
